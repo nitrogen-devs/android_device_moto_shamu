bf938c63354d23f91279da4153cb7ac8be97e5e5
Valeriy <binder_z@mail.ru>
2016-07-17 01:10:58 +0400

    Removed sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,8 +124,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/moto/shamu
 # Support Native Layer RF cutback
 BOARD_USES_CUTBACK_IN_RILD := true
 
-BOARD_SEPOLICY_DIRS += device/moto/shamu/sepolicy
-
 HAVE_ADRENO_SOURCE:= false
 
 OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
